753188e0f435ce8340f8f72a1a6aa34cdb75a71a
aegroto <aegroto@protonmail.com>
2024-11-28 20:16:43 +0100

    chore: improved filter unit testing code

--- modules/name_filter/__tests/test_base.py
+++ modules/name_filter/__tests/test_base.py
@@ -8,24 +8,43 @@ DEFAULT_TOKEN = "TEST:TOKEN"
 DEFAULT_CHAT_ID = 1
 DEFAULT_USER_ID = 1000
 
-@pytest.mark.asyncio
-async def test_no_block():
-    block_expressions = [".*drug.*"]
-    mock_session = TelegramHTTPSession(DEFAULT_TOKEN)
-    mock_session.request = AsyncMock()
-    filter = NameFilterHandler(mock_session, block_expressions)
 
-    await filter.handle({
+class MockSession(TelegramHTTPSession):
+    def __init__(self):
+        self.request = AsyncMock()
+
+    def expect_ban(self, chat_id=DEFAULT_CHAT_ID, user_id=DEFAULT_USER_ID):
+        self.request.assert_called_with(
+            "POST",
+            "banChatMember",
+            data={"chat_id": chat_id, "user_id": user_id},
+        )
+
+
+def build_update(first_name, username):
+    return {
         "message": {
-            "chat": {
-                "id": DEFAULT_CHAT_ID
-            },
+            "chat": {"id": DEFAULT_CHAT_ID},
             "new_chat_member": {
                 "id": DEFAULT_USER_ID,
-                "first_name": "Drugo drago",
-                "username": "thedrugo",
-            }
+                "first_name": first_name,
+                "username": username,
+            },
         }
-    })
+    }
 
-    mock_session.request.assert_called_with()
+
+@pytest.mark.parametrize(
+    "first_name,username,block_expressions", [
+        ("Drugo drago", "thedrugo", [".*drug.*"]),
+        ("Kitty freesex", "kittypig", [".*sex.*"]),
+    ]
+)
+@pytest.mark.asyncio
+async def test_block(first_name, username, block_expressions):
+    mock_session = MockSession()
+    filter = NameFilterHandler(mock_session, block_expressions)
+
+    await filter.handle(build_update(first_name, username))
+
+    mock_session.expect_ban()
